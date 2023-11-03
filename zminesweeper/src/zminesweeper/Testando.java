518d0cccd75c02fb0728115ecf7378bac6ebcc90
odeni3 <danielgpmeneses@gmail.com>
2023-11-03 16:32:35 -0300

    testando commit...

--- zminesweeper/src/zminesweeper/Testando.java
+++ zminesweeper/src/zminesweeper/Testando.java
@@ -5,6 +5,7 @@ public class Testando {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!!!");
 		System.out.println("Jajá começamos o projeto.");
+		System.out.println("Testando...");
 
 	}
 
