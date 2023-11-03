2c2068d97b06793e545088fe879eb13fe1fb48db
MarkDosProgramas <mvfranca2004@gmail.com>
2023-11-03 17:54:15 -0300

    adicionando atributos da classe celula

---
+++ zminesweeper/src/zminesweeper/Celula.java
@@ -0,0 +1,5 @@
+package zminesweeper;
+
+public class Celula {
+
+}
--- zminesweeper/src/zminesweeper/Main.java
+++ zminesweeper/src/zminesweeper/Main.java
@@ -1,6 +1,6 @@
 package zminesweeper;
 
-public class Testando {
+public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!!!");
